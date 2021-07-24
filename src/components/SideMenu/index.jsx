2f4ec372e3dc07d383c717666a05b9dcfc8df435
xxl <280516895@qq.com>
2021-07-24 09:45:20 +0800

    SideMenu修改完成

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,71 +1,67 @@
-import React from 'react'
-import {withRouter} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import style from './index.module.css'
+import axios from 'axios'
 
 const { Sider } = Layout
 const { SubMenu } = Menu;
-
-const menuList = [
-  {
-    key: '/home',
-    title: '首页',
-    icon: <UserOutlined/>
-  },
-  {
-    key:'/user-manage',
-    title: '用户管理',
-    icon:<UserOutlined/>,
-    children: [
-      {
-        key:'/user-manage/list',
-        title:'用户管理',
-        icon:<UserOutlined/>
-      }    
-    ]
-  },
-  {
-    key:'/right-manage',
-    title:'权限管理',
-    icon:<UserOutlined/>,
-    children:[
-      {
-        key:'/right-manage/role/list',
-        title:'角色列表',
-        icon:<UserOutlined/>
-      },
-      {
-        key:'/right-manage/right/list',
-        title:'权限列表',
-        icon:<UserOutlined/>
-      }
-    ]
-  }
-]
+const iconList = {
+  '/home': <UserOutlined />,
+  '/user-manage': <UserOutlined />,
+  '/user-manage/list': <UserOutlined />,
+  '/right-manage': <UserOutlined />,
+  '/right-manage/role/list': <UserOutlined />,
+  '/right-manage/right/list': <UserOutlined />
+}
 
 function SideMenu(props) {
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:8000/rights?_embed=children').then(
+      (res) => {
+        console.log(res.data);
+        setMenu(res.data)
+      }
+    )
+  }, [])
+  const checkPagePermission = (item) => {
+    return item.pagepermisson
+  }
   const renderMenu = (menuList) => {
-    return menuList.map(item=>{
-      const {key,title,icon} = item
-      if(item.children){        
-        return <SubMenu key={key} icon={icon} title={title}>
+    return menuList.map(item => {
+      const { key, title } = item
+      // 渲染二级菜单
+      if (item.children?.length > 0 && checkPagePermission(item)) {
+        return <SubMenu key={key} icon={iconList[item.key]} title={title}>
           {renderMenu(item.children)}
         </SubMenu>
-      } 
-      return <Menu.Item key={key} icon={icon} onClick={() => {
+      }
+      //渲染一级菜单
+      return checkPagePermission(item) && <Menu.Item key={key} icon={iconList[item.key]} onClick={() => {
         props.history.push(key)
         // console.log(props);
       }}>{title}</Menu.Item>
     })
   }
+  //设置刷新后扔高亮
+  const selectKeys = [props.location.pathname]
+  //设置刷新后一级菜单展开
+  const openKeys = ['/'+props.location.pathname.split('/')[1]]
+
   return (
 
     <Sider trigger={null} collapsible collapsed={false}>
-      <div className={style.logo}>新闻发布管理系统</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        {renderMenu(menuList)}
-      </Menu>
+      <div style={{display:'flex',flexDirection:'column',height:'100%'}}>
+        <div className={style.logo}>新闻发布管理系统</div>
+        <div style={{flex:1,overflow:"auto"}}>
+          <Menu theme="dark" mode="inline" 
+          selectedKeys={selectKeys} defaultOpenKeys={openKeys}>
+            {renderMenu(menu)}
+          </Menu>
+        </div>
+      </div>
     </Sider>
 
   )
